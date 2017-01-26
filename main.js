c3e027b13b8a9882831bb50da9a01bf05131cfef
Jakub Pazdyga <admin@lascalia.com>
2017-01-26 11:38:16 +0100

    Firther improvements

--- main.js
+++ main.js
@@ -1,51 +1,25 @@
 require('shelljs/global');
+require('should');
 
-var hierafile = "/etc/puppet/hieradata/production/common.yaml";
-
+var vlist = require('./variables.js');
+var parse = require('csv-parse/lib/sync');
 var https = require('https');
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser')
 var objectPath = require('object-path');
 
-const yaml = require('js-yaml');
-const fs = require('fs');
+var desc_array  = [];
 
-try {
-    var config = yaml.safeLoad(fs.readFileSync(hierafile, 'utf8'));
-} catch (e) {
-    console.log(e);
-}
+global.hierafile;
 
-app.get('/', function(req, res) {
-  res.type('text/plain'); // set content-type
-  res.send('This a yaml config files update application\n'); // send text response
-});
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
 
-app.get('/:param/:value', function(req, res) {
-var p = req.params.param;
-var q = req.params.value;
-var resp = "Setting parameter \"" + p + "\" with value of \"" + q + "\"\n";
-
-var value = p;
-oldvalue = objectPath.get(config, value);
-//console.log('Old value: ' + oldvalue);
-
-newvalue = objectPath.set(config, value, q);
-
-//console.log(config);
-
-try {
-    const write = yaml.safeDump(config);
-//    console.log(write);
-    fs.writeFile(hierafile, write, function(err) {
-      if(err) {
-        return console.log(err);
-      }
-    console.log("The file was saved!");
-    });
-} catch (e) {
-    console.log(e);
-}
+var yaml = require('js-yaml');
+var fs = require('fs');
 
 app.use(function (req, res, next) {
 
@@ -66,13 +40,262 @@ app.use(function (req, res, next) {
     next();
 });
 
+/*
+Construct response for an empty request
+*/
+
+app.get('/', function(req, res) {
+  res.type('text/plain'); // set content-type
+  res.send('This a yaml config files update application\n'); // send text response
+});
+
+/*
+Construct response for existing value request
+*/
+
+app.post('/:param', function(req, res) {
+
+var h = req.body.hostgroup;
+var p = req.params.param;
+
+global.hierafile = "/etc/puppet/hieradata/production/" + h + ".yaml";
+try {
+    var config = yaml.safeLoad(fs.readFileSync(global.hierafile, 'utf8'));
+} catch (e) {
+    console.log(e);
+};
+
+if (p.split('.').length - 1 > 1){
+
+  var result = exec('echo ' + p, {silent:true}).exec('awk -F. \'{print $1, substr($2,2)}\' FPAT=\'(^[^.]+)|([.].*)\'', {silent:true}).exec('sed \'s/\\(.*\\)\\./\\1 /\'', {silent:true}).exec('tr -d "\n"', {silent:true});
+  var result1 = exec('echo ' + result, {silent:true}).exec('awk \'{print $1}\'', {silent:true}).exec('tr -d "\n"', {silent:true});
+  var result2 = exec('echo ' + result, {silent:true}).exec('awk \'{print $2}\'', {silent:true}).exec('tr -d "\n"', {silent:true});
+  var result3 = exec('echo ' + result, {silent:true}).exec('awk \'{print $3}\'', {silent:true}).exec('tr -d "\n"', {silent:true});
+
+  oldvalue = objectPath.get(config, [result1, result2, result3]);
+  //var h = global.hierafile
+  if ( oldvalue == null || typeof oldvalue == 'undefined' ) {
+    dots = p.indexOf('.') !== -1;
+    if ( dots == true ) {
+      //var pattern = exec('echo ' + p, {silent:true}).exec('awk -F\'.\' \"{print $1}\"', {silent:true});
+      var splitted = p.split(".");
+      var pattern = splitted[0];
+    }
+    else
+    {
+      var pattern = p;
+    }
+    var search = exec('find /etc/puppet/hieradata/ -name \"*.yaml\" -exec grep -H \"' + pattern + '\" {} \\;', {silent:true,timeout:5000}).exec('awk -F: \'{print $1}\'', {silent:true,timeout:5000}).exec('tail -1', {silent:true,timeout:5000}).exec('tr -d "\n"', {silent:true,timeout:5000});
+    if (search) {
+      oldvalue="No such parameter found.";
+    };
+
+    hierafile = search.stdout;
+    console.log("Hierafile: " + hierafile);
+    try {
+      var config = yaml.safeLoad(fs.readFileSync(hierafile, 'utf8'));
+    } catch (e) {
+      console.log(e);
+    };
+    oldvalue = objectPath.get(config, [result1, result2, result3]);
+    var h = search.stdout;
+  }
+}
+else
+{
+  var result = p;
+  oldvalue = objectPath.get(config, [result]);
+  
+  var h = hierafile
+  console.log("hierafile: " + h);
+
+  console.log("Oldvalue:" + oldvalue );
+
+  if ( oldvalue == null || typeof oldvalue == 'undefined' ) {
+    var search = exec('find /etc/puppet/hieradata/ -name \"*.yaml\" -exec grep -H \"' + result + '\" {} \\;', {silent:true,timeout:5000}).exec('awk -F: \'{print $1}\'', {silent:true,timeout:5000}).exec('tr -d "\n"', {silent:true,timeout:5000});
+    if (search) {
+      oldvalue="No such parameter found.";
+    };
+    var h = search.stdout;
+    console.log("search.stdout: " + h);
+    try {
+      var config = yaml.safeLoad(fs.readFileSync(h, 'utf8'));
+    } catch (e) {
+      console.log(e);
+    };
+    oldvalue = objectPath.get(config, [result]);
+    var h = search.stdout;
+  }
+}
+
+console.log("Responding with the existing value '" + oldvalue + "' for variable '" + p + "'" + " in path " + h + ".");
+if (pattern) {
+  p = pattern;
+}
+console.log("H: " + h);
+console.log("P: " + p);
+try {
+  var predesc = exec('grep -B1 \"' + p + '\" ' + h, {silent:true,timeout:5000}).exec('grep \"^#\"', {silent:true,timeout:5000}).exec('egrep -v \"^' + p + '*:*\"', {silent:true,timeout:5000});
+} catch(e) {
+  console.log(e);
+};
+console.log("Predesc:" + predesc);
+if (predesc) {
+  var predescription = predesc.split("#");
+  var description = predescription[1];
+};
+
+if ( description == null || typeof description == 'undefined' ) {
+  var description = "No description available.";
+};
+
+var j = {dependency:[], found:[{"backend":"YAML","path":h,"url":""}],"value":oldvalue,"description":description};
+//console.log("J: " + JSON.stringify(j));
+global.hierafile = h;
+
+res.send(j);
+//delete pattern;
+//delete h;
+});
+
+/*
+Construct response for value change request
+*/
+
+app.post('/:param/:value', function(req, res) {
+
+// Debug the whole request:
+// console.log(res.send(JSON.stringify(req.body, null, 4)));
+
+var h = global.hierafile;
+var p = req.params.param;
+var q = req.params.value;
+
+//console.log("Hierafile: " + h);
+
+  if (h) {
+    h = global.hierafile;
+  }
+  try {
+    var config = yaml.safeLoad(fs.readFileSync(h, 'utf8'));
+  } catch (e) {
+    console.log(e);
+  };
+
+var get_comments = exec('grep -in \"^#\" ' + global.hierafile, {silent:true,timeout:5000}).exec('grep .', {silent:true,timeout:5000});
+console.log('get_comments_1:' + get_comments);
+
+if (p.split('.').length - 1 > 1){
+
+  var result = exec('echo ' + p, {silent:true}).exec('awk -F. \'{print $1, substr($2,2)}\' FPAT=\'(^[^.]+)|([.].*)\'', {silent:true}).exec('sed \'s/\\(.*\\)\\./\\1 /\'', {silent:true}).exec('tr -d "\n"', {silent:true});
+
+  var result1 = exec('echo ' + result, {silent:true}).exec('awk \'{print $1}\'', {silent:true}).exec('tr -d "\n"', {silent:true});
+  var result2 = exec('echo ' + result, {silent:true}).exec('awk \'{print $2}\'', {silent:true}).exec('tr -d "\n"', {silent:true});
+  var result3 = exec('echo ' + result, {silent:true}).exec('awk \'{print $3}\'', {silent:true}).exec('tr -d "\n"', {silent:true});
+
+  oldvalue = objectPath.get(config, [result1, result2, result3]);
+  newvalue = objectPath.set(config, [result1, result2, result3], q);
+}
+else if (p.split('.').length - 1 == 1) 
+{
+  var result = exec('echo ' + p, {silent:true}).exec('awk -F. \'{print $1, substr($2,2)}\' FPAT=\'(^[^.]+)|([.].*)\'', {silent:true}).exec('sed \'s/\\(.*\\)\\./\\1 /\'', {silent:true}).exec('tr -d "\n"', {silent:true});
+
+  var result1 = exec('echo ' + result.stdout, {silent:true}).exec('awk \'{print $1}\'', {silent:true}).exec('tr -d "\n"', {silent:true});
+  var result2 = exec('echo ' + result.stdout, {silent:true}).exec('awk \'{print $2}\'', {silent:true}).exec('tr -d "\n"', {silent:true});
+
+  var tree1 = result1.stdout; 
+  var tree2 = result2.stdout;
+
+  var modifies = tree1 + "." + tree2;
+  oldvalue = objectPath.get(config, [modifies]);
+  newvalue = objectPath.set(config, [modifies], q);
+}
+else
+{
+  var result = p;
+  oldvalue = objectPath.get(config, result);
+  newvalue = objectPath.set(config, result, q);
+}
+
+try {
+  var desc_array = [];
+  var prerecords;
+  var write = yaml.safeDump(config);
+  //console.log("Data to write: " + write);
+  fs.writeFile(global.hierafile, write, function(err) {
+    if(err) {
+      return console.log(err);
+    }
+    
+    var data = fs.readFileSync(global.hierafile).toString().split("\n");
+    //console.log("Data:" + data);
+    if (get_comments) {
+      var comments = get_comments.toString().split("\n");
+      //var data = comments;
+      for (i in comments) {
+        var line = comments[i];
+        //console.log("Comments Line:" + line);
+        if (line) {
+          var preproc = exec('echo ' + line, {silent:true}).exec('sed \'s\/^\/"\/g\' ', {silent:true}).exec('sed \'s\/$/\"\/g\'', {silent:true}).exec('sed \'s\/:#\/\":\"#/g\'', {silent:true});
+          desc_array.push(preproc);
+        };
+      };
+    };
+    //console.log("Is array: " + Array.isArray(desc_array));
+ 
+    var prerecords = desc_array.join("");
+
+    //console.log("Prerecords: " + prerecords);
+
+    var records = parse(prerecords, {delimiter:':', rowDelimiter: '\n', columns:['linenr', 'text']});
+
+    //console.log("Records: " + records);
+
+    for (var i = 0, len = records.length; i < len; i++) {
+      linenr = (records[i].linenr - 1);
+      //console.log("Linenr: " + linenr);
+      text = (records[i].text);
+      //console.log("Text: " + text);
+      data.splice(linenr, 0, text);
+      //console.log("Test: " + test);
+      var file_contents = data.join("\n");
+    }
+
+    //console.log("file_contents: " + file_contents);
+
+    if ( get_comments.indexOf('#') > -1 ) {
+      console.log("There are comments to inject.");
+      try {
+          var close = fs.writeFileSync(global.hierafile, file_contents);
+        } catch(e) {
+          console.log(e);
+        };
+    }
+  
+    console.log("The file was saved! Changed value '" + oldvalue + "' to '" + q + "' for variable '" + p + "'.");
+  });
+} catch(e) {
+    console.log(e);
+}
+
+var resp = oldvalue;
+
 res.send(resp);
 
 });
+
+var json_vlist = vlist.fn_json_allvars();
+
+app.get('/list_hiera', function(req, res) {
+  console.log("Getting the list of existing hiera variables.");
+  res.send(json_vlist);
+});
+
+console.log("Starting http server...");
 
 https.createServer({
   key: fs.readFileSync('key.pem'),
   cert: fs.readFileSync('cert.pem')
 }, app).listen(4433);
 
-//app.listen(process.env.PORT || 8088);
+console.log("Up and running.");
---
+++ variables.js
@@ -0,0 +1,50 @@
+require('shelljs/global');
+require('should');
+
+var parse = require('csv-parse/lib/sync');
+var https = require('https');
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var objectPath = require('object-path');
+
+var yaml_array=[];
+global.json_allvars = [];
+
+module.exports = {
+
+  fn_json_allvars: function () {
+//    function fn_json_allvars() {
+    var search = exec('find /etc/puppet/hieradata/ -name \"*.yaml\"', {silent:true,timeout:5000});
+    yaml_array = search.toString().split("\n");
+    n=0; 
+    yaml_array.forEach(function(v) {
+      //console.log("Here:" + v);
+      if (v) {
+        var variablename = grep('^.[a-z]', v).exec('awk -F": " \'{print $1}\'', {silent:true,timeout:5000}).exec('sed \'s/:$//g\'', {silent:true,timeout:5000});
+    //.exec('tr "\n" ","', {silent:true,timeout:5000});
+        var varstring = variablename.stdout.slice(0, -1).toString().split("\n");
+    //    console.log(Array.isArray(varstring));
+        n++;
+//        json_singlevar = "\"dataset" + n + "\":{\"location\":\"" + v + "\",\"variable\":\"" + varstring + "\"},";
+          json_singlevar = "{\"location\": \"" + v + "\", \"variable\":\"" + varstring + "\"},";
+    //    console.log(json_singlevar);
+        global.json_allvars = global.json_allvars + json_singlevar;
+
+    /*
+          varstring.forEach(function(x) {
+          console.log(v, x);
+        });
+    */
+  
+      }
+    });
+    pre_global_json_allvars = "[" + global.json_allvars.slice(0, -1) + "]";
+    json = JSON.parse(pre_global_json_allvars);
+    return json;
+//  };
+  }
+};
+
+//fn_json_allvars();
+//console.log("Test: " + global.json_allvars);
